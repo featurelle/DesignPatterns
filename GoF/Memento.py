51d1417a800889bea0480efa13f243bb68e298b3
pointuseless <s3m.channel@gmail.com>
2020-12-17 20:34:28 +0200

    ok

--- GoF/Memento.py
+++ GoF/Memento.py
@@ -1,5 +1,5 @@
 """This code demonstrates a possible implementation of the Memento pattern in Python
-Let's say we want to have a story of some actions and we also want to be able to undo and redo our actions"""
+Let's say we want to have a story of some actions and we also want to be able to undo our actions"""
 
 
 class Memento:
@@ -22,11 +22,14 @@ class TextEditor:
 
 
 class History:
+    def __init__(self):
+        self.states = list()
+
     def save(self, editor_):
-        self.obj = editor_.save()
+        self.states.append(editor_.save())
 
     def undo(self, editor_):
-        editor_.undo(self.obj)
+        editor_.undo(self.states.pop())
 
 
 if __name__ == '__main__':
@@ -34,12 +37,21 @@ if __name__ == '__main__':
 
     editor = TextEditor()
 
+    editor.write("Hi")
+    print(editor.content + '\n')
+
+    history.save(editor)
+
     editor.write("Hello world")
     print(editor.content + '\n')
 
     history.save(editor)
 
     editor.write("How are you?")
+
+    print(editor.content + '\n')
+
+    history.undo(editor)
 
     print(editor.content + '\n')
 
