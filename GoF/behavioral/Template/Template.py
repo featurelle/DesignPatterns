68d11906e66650f932d9c17eb5ea38d315539e6c
pointuseless <s3m.channel@gmail.com>
2020-12-18 22:53:02 +0200

    ok

---
+++ GoF/behavioral/Template/Template.py
@@ -0,0 +1,78 @@
+from abc import ABC, abstractmethod
+
+
+class AverageCalculator(ABC):
+
+    def average(self):
+        try:
+            num_items = 0
+            total_sum = 0
+            while self.has_next():
+                total_sum += self.next_item()
+                num_items += 1
+
+            return total_sum / num_items
+
+        except ZeroDivisionError:
+            raise RuntimeError("Can not calculate the average of an empty sequence.txt")
+
+        finally:
+            self.dispose()
+
+    @abstractmethod
+    def has_next(self):
+        pass
+
+    @abstractmethod
+    def next_item(self):
+        pass
+
+    def dispose(self):
+        pass
+
+
+class FileAverageCalculator(AverageCalculator):
+
+    def __init__(self, file):
+        self.file = file
+        self.last_line = self.file.readline()
+
+    def has_next(self):
+        return self.last_line != ''
+
+    def next_item(self):
+        item = float(self.last_line)
+        self.last_line = self.file.readline()
+        return item
+
+    def dispose(self):
+        self.file.close()
+
+
+class ListAverageCalculator(AverageCalculator):
+
+    def __init__(self, seq):
+        self.seq = seq
+
+    def has_next(self):
+        return bool(self.seq)
+
+    def next_item(self):
+        return self.seq.pop()
+
+
+def demo():
+
+    seq = [19, 20, 11, 0, 5, 7, 3]
+    seq_file = open('sequence.txt')
+
+    calc1 = FileAverageCalculator(seq_file)
+    print('Average from a file: ', calc1.average())
+
+    calc2 = ListAverageCalculator(seq)
+    print('Average from a list: ', calc2.average())
+
+
+if __name__ == "__main__":
+    demo()
+
