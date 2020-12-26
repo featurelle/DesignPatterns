b794edd6ca71eb55a67fc28af965745abec27b1a
pointuseless <s3m.channel@gmail.com>
2020-12-26 21:59:05 +0200

    Whole bnew State pattern

--- GoF/behavioral/State.py
+++ GoF/behavioral/State.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 
-# Один абстрактный стейт
+# А если еще стэп добавить, то классу вообще пиздец настанет)
+# Надо как-то иначе подумать)
+
+
+# Один абстрактный стейт, который в свою очередь тоже имеет Свое состояние - конечность и бесконечность!
 class IteratorState(ABC):
 
     def __init__(self, iterator: ListIterator):
@@ -18,6 +22,61 @@ class IteratorState(ABC):
     @abstractmethod
     def inverse(self):
         pass
+
+    @abstractmethod
+    def accept(self, v: StateVisitor):
+        pass
+
+
+class StateVisitor(ABC):
+    # Обязанности Стейта:
+    def __init__(self, iterator: ListIterator):
+        self.iterator = iterator
+
+    @abstractmethod
+    def toggle_finite(self):
+        pass
+
+    # Обязанности Визитора:
+    @abstractmethod
+    def forward(self, state: ForwardDirectionIter):
+        pass
+
+    @abstractmethod
+    def backward(self, state: BackwardDirectionIter):
+        pass
+
+
+class FiniteStateVisitor(StateVisitor):
+
+    def forward(self, state: ForwardDirectionIter):
+        return state.next()
+
+    def backward(self, state: BackwardDirectionIter):
+        return state.next()
+
+    def toggle_finite(self):
+        self.iterator.finite = InfiniteStateVisitor(self.iterator)
+
+
+class InfiniteStateVisitor(StateVisitor):
+
+    def forward(self, state: ForwardDirectionIter):
+        try:
+            return state.next()
+        except StopIteration:
+            state.iterator.current = 0
+            return state.next()
+
+    def backward(self, state: BackwardDirectionIter):
+        try:
+            return state.next()
+        except StopIteration:
+            state.iterator.current = len(state.iterator.seq) - 1
+            return state.next()
+
+    def toggle_finite(self):
+        self.iterator.finite = FiniteStateVisitor(self.iterator)
 
 
 # Конкретные Стейты с разными функциями
@@ -40,6 +99,9 @@ class ForwardDirectionIter(IteratorState):
     def inverse(self):
         self.iterator.direction = BackwardDirectionIter(self.iterator)
 
+    def accept(self, v: StateVisitor):
+        return v.forward(self)
+
 
 class BackwardDirectionIter(IteratorState):
 
@@ -60,19 +122,8 @@ class BackwardDirectionIter(IteratorState):
     def inverse(self):
         self.iterator.direction = ForwardDirectionIter(self.iterator)
 
-
-
-
-class FiniteIter(IteratorState):
-
-    def next(self):
-        # Обращаюсь к методу Итератора, а не другого вида Стейт, таким образом позволяю менять виды Стейт независимо
-        if self.iterator.has_next():
-            self.iterator.next()
-        else:
-            self.ite
-
-
+    def accept(self, v: StateVisitor):
+        return v.backward(self)
 
 
 class ListIterator:
@@ -81,15 +132,51 @@ class ListIterator:
         self.seq = seq
         self.current = 0
         self.direction = ForwardDirectionIter(self)
+        self.finite = FiniteStateVisitor(self)
 
     def next(self):
-        self.direction
+        return self.direction.accept(self.finite)
 
     def has_next(self):
-        self.direction.has_next()
+        return self.direction.has_next()
 
     def change_direction(self):
-        pass
+        self.direction.inverse()
 
     def toggle_finite(self):
-        pass
+        self.finite.toggle_finite()
+
+
+def demo():
+    sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    iterator = ListIterator(sequence)
+
+    while True:
+        try:
+            print(iterator.next())
+        except StopIteration:
+            print('Switching direction!')
+            iterator.change_direction()
+            break
+
+    # Работает через жопу, поменять!
+    while True:
+        try:
+            print(iterator.next())
+        except StopIteration:
+            print('Now making it infinite!')
+            iterator.toggle_finite()
+            break
+
+    for _ in range(100):
+        print(iterator.next())
+    else:
+        print('Now backwards again in infinite mode!')
+        iterator.change_direction()
+
+    for _ in range(100):
+        print(iterator.next())
+
+
+if __name__ == "__main__":
+    demo()
