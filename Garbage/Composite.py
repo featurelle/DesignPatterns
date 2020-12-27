7d94f642962f9a91f4eab74c70f138a2afb06996
pointuseless <s3m.channel@gmail.com>
2020-12-27 18:05:55 +0200

    Composite full refactor, old versions were moved to Garbage

---
+++ Garbage/Composite.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+import random
+from abc import ABC, abstractmethod
+
+# Другой взгляд на задачу, описанную у меня в Итераторе
+
+
+class TownComponent(ABC):
+
+    @abstractmethod
+    def get_nested(self):
+        pass
+
+    @abstractmethod
+    def add(self, nested: TownComponent):
+        pass
+
+    @abstractmethod
+    def remove(self, nested: TownComponent):
+        pass
+
+    @abstractmethod
+    def windows(self) -> list:
+        pass
+
+
+class Window(TownComponent):
+
+    def __init__(self, width: int, length: int, plastic: bool = False):
+        self.width = width
+        self.length = length
+        self.plastic = plastic
+
+
+
+class Composite(TownComponent):
+
+    def __init__(self, children: TownComponent):
+        self.children
+
+
+class Section(Composite):
+    pass
+
+
+class Floor(Composite):
+    pass
+
+
+class OldHouse(Composite):
+
+    def __init__(self, floors: int, windows_per_floor: int):
+        self.floors = list()
+        for _ in range(floors):
+            plastic = random.choice([False] * 2 + [True] * 5)   # 5 к 2, что на этаже пластиковые окна
+            self.floors.append([Window(300, 200, plastic) for _ in range(windows_per_floor)])
+
+
+class VeryOldHouse(Composite):
+
+    def __init__(self, floors: int, windows_per_floor: int):
+        self.floors = dict()
+        for i in range(floors):
+            plastic = random.choice([False] * 9 + [True] * 2)   # 9 к 2, что на этаже деревянные окна
+            self.floors[i + 1] = ([Window(200, 150, plastic) for _ in range(windows_per_floor)])
+
+
+class AdministrativeBuilding(Composite):
+    pass
+
+
+class Town(Composite):
+    pass
+
+
+class Country(Composite):
+    pass
--- GoF/sctructural/Composite/Composite.py
+++ GoF/sctructural/Composite/Composite.py
@@ -1,70 +1,178 @@
-import random
-from abc import ABC, abstractmethod
-
-# Другой взгляд на задачу, описанную у меня в Итераторе
+from __future__ import annotations
 
 
-class TownComponent(ABC):
+class FileSystem:
 
-
-    def get_nested(self):
-        return []
-
-
-    def add(self):
+    def full_path(self) -> str:
         raise NotImplementedError
 
-    def
+    def size(self) -> int:
+        raise NotImplementedError
+
+    def show(self) -> str:
+        raise NotImplementedError
 
 
-class Window(TownComponent):
+class Directory(FileSystem):
 
-    def __init__(self, width: int, length: int, plastic: bool = False):
-        self.width = width
-        self.length = length
-        self.plastic = plastic
+    def __init__(self, root: Directory, name: str, visible: bool = True):
+        self.root = root
+        self.name = name
+        self.leaves = []
+        self._visible = visible
+
+    def create_folder(self, name: str):
+        new_folder = Directory(self, name)
+        self.leaves.append(new_folder)
+        return new_folder
+
+    def create_file(self, name: str):
+        new_file = File(self, name, str())
+        self.leaves.append(new_file)
+        return new_file
+
+    def add(self, leaf: FileSystem):
+        self.leaves.append(leaf)
+
+    def full_path(self) -> str:
+        return self.root.full_path() + '/' + self.name
+
+    def show(self, indent=0) -> str:
+        indentation = '----' * indent
+        string = indentation + self.name + '\n'
+        for leaf in self.leaves:
+            if leaf.visible:
+                string += indentation + leaf.show(indent + 1) + '\n'
+        return string
+
+    def size(self) -> int:
+        total = 0
+        for leaf in self.leaves:
+            total += leaf.size()
+        return total
+
+    def delete(self, leaf: FileSystem):
+        try:
+            self.leaves.remove(leaf)
+        except ValueError:
+            print('Cannot delete non-existing leaf')
+
+    def move(self, directory: Directory):
+        directory.add(self)
+        self.root.delete(self)
+        self.root = directory
+
+    @property
+    def visible(self) -> bool:
+        return self._visible
+
+    @visible.setter
+    def visible(self, visible: bool):
+        self._visible = visible
 
 
-class Composite(TownComponent):
+class SystemRoot(Directory):
 
-    def __init__(self, children: TownComponent):
-        self.children
+    __instance = None
 
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = object.__new__(cls)
+        return cls.__instance
 
-class Section(Composite):
-    pass
+    def __init__(self):
+        super().__init__(self, '%%')
 
+    def full_path(self) -> str:
+        return self.name
 
-class Floor(Composite):
-    pass
+    @property
+    def visible(self) -> bool:
+        return True
 
-
-class OldHouse(Composite):
-
-    def __init__(self, floors: int, windows_per_floor: int):
-        self.floors = list()
-        for _ in range(floors):
-            plastic = random.choice([False] * 2 + [True] * 5)   # 5 к 2, что на этаже пластиковые окна
-            self.floors.append([Window(300, 200, plastic) for _ in range(windows_per_floor)])
-
-
-class VeryOldHouse(Composite):
-
-    def __init__(self, floors: int, windows_per_floor: int):
-        self.floors = dict()
-        for i in range(floors):
-            plastic = random.choice([False] * 9 + [True] * 2)   # 9 к 2, что на этаже деревянные окна
-            self.floors[i + 1] = ([Window(200, 150, plastic) for _ in range(windows_per_floor)])
+    @visible.setter
+    def visible(self, value):
+        self._visible = value
 
 
-class AdministrativeBuilding(Composite):
-    pass
+class File(FileSystem):
+
+    def __init__(self, root: Directory, name: str, inside: str, visible: bool = True):
+        self.root = root
+        self.name = name
+        self.inside = inside
+        self._visible = visible
+
+    def edit(self, inside):
+        self.inside = inside
+
+    def open(self):
+        print('>>> ' + self.name + ' : ' + self.inside)
+
+    def full_path(self) -> str:
+        return self.root.full_path() + '/' + self.name
+
+    def show(self, indent=0) -> str:
+        return '----' * indent + self.name
+
+    def move(self, directory: Directory):
+        directory.add(self)
+        self.root.delete(self)
+        self.root = directory
+
+    @property
+    def visible(self) -> bool:
+        return self._visible
+
+    @visible.setter
+    def visible(self, visible: bool):
+        self._visible = visible
+
+    def size(self) -> int:
+        return len(self.inside)
 
 
-class Town(Composite):
-    pass
+def demo():
+
+    root = SystemRoot()
+    photos = root.create_folder('Photos')
+    my = photos.create_folder('My')
+    my.create_file('Empty.txt')
+    vacation = File(my, 'Vacation.img', 'My vacation photo')
+    my.add(vacation)
+
+    print(my.show())
+    print(photos.show())
+    print(root.show())
+    print(vacation.full_path())
+    print(root.full_path())
+
+    print()
+    my.visible = False
+    print(root.show())
+    my.visible = True
+
+    new = photos.create_folder('New_folder')
+    print(root.show())
+
+    ott = new.create_folder('123')
+    file = ott.create_file('new_text.txt')
+    file.edit('HELLO WORLD!!!!!!')
+    print(str(root.size()) + ' bytes\n')
+    print(root.show())
+
+    print('\nNow moving in another directory\n')
+    ott.move(my)
+    print(root.show())
+
+    print('All: ' + str(root.size()) + ' bytes\n')
+    print('new_text: ' + str(file.size()) + ' bytes\n')
+    ott.delete(file)
+    print('Now deleting this file')
+    print(root.show())
+    print('All: ' + str(root.size()) + ' bytes\n')
 
 
-class Country(Composite):
-    pass
+if __name__ == '__main__':
 
+    demo()
