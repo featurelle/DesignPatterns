45c0375b1f5cc6bc05bb701a2a0af90c92a0f7e6
pointuseless <s3m.channel@gmail.com>
2020-12-28 13:59:11 +0200

    Math parser with chain completed!

---
+++ GoF/behavioral/Chain/Chain.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+import re
+
+
+NUMBER = r'-?\d+(\.\d+)?'
+
+
+class MathParser:
+
+    def __init__(self, then: MathParser = None):
+        self._then = then
+
+    def parse(self, math: str):
+        raise NotImplementedError
+
+    def then(self, parser: MathParser):
+        self._then = parser
+
+
+class BracketsParser(MathParser):
+
+    def parse(self, math: str):
+        search = re.search(r'\([0-9 +*/.-]+\)', math)
+        if search:
+            match = search.group(0).strip('()')
+            result = self._then.parse(match)
+            modified = math.replace('(' + match + ')', str(result))
+            return self.parse(modified)
+        else:
+            return float(self._then.parse(math))
+
+
+class Substraction(MathParser):
+
+    def parse(self, math: str):
+        search = re.search(fr'{NUMBER} - {NUMBER}', math)
+        if search:
+            match = search.group(0)
+            x = float(re.search(fr'^{NUMBER}', match).group(0))
+            y = float(re.search(fr'{NUMBER}$', match).group(0))
+            result = x - y
+            modified = math.replace(match, str(result))
+            return self.parse(modified)
+        else:
+            return float(self._then.parse(math))
+
+
+class Addition(MathParser):
+
+    def parse(self, math: str):
+        search = re.search(fr'{NUMBER} \+ {NUMBER}', math)
+        if search:
+            match = search.group(0)
+            x = float(re.search(fr'^{NUMBER}', match).group(0))
+            y = float(re.search(fr'{NUMBER}$', match).group(0))
+            result = x + y
+            modified = math.replace(match, str(result))
+            return self.parse(modified)
+        else:
+            return float(self._then.parse(math))
+
+
+class Multiplication(MathParser):
+
+    def parse(self, math: str):
+        search = re.search(fr'{NUMBER} \* {NUMBER}', math)
+        if search:
+            match = search.group(0)
+            x = float(re.search(fr'^{NUMBER}', match).group(0))
+            y = float(re.search(fr'{NUMBER}$', match).group(0))
+            result = x * y
+            modified = math.replace(match, str(result))
+            return self.parse(modified)
+        else:
+            return float(self._then.parse(math))
+
+
+class Division(MathParser):
+
+    def parse(self, math: str):
+        search = re.search(fr'{NUMBER} [/:] {NUMBER}', math)
+        if search:
+            match = search.group(0)
+            x = float(re.search(fr'^{NUMBER}', match).group(0))
+            y = float(re.search(fr'{NUMBER}$', match).group(0))
+            result = x / y
+            modified = math.replace(match, str(result))
+            return self.parse(modified)
+        else:
+            return float(self._then.parse(math))
+
+
+class ErrorHandler(MathParser):
+
+    def parse(self, math: str):
+        if not re.fullmatch(fr'{NUMBER}', math):
+            raise RuntimeError('Invalid syntax!')
+        else:
+            return float(math)
+
+
+math_str = '(3 - 15 - (20 - ((12 - 2 * (20 / (10 + 4))) - 14)) - -(10 - 2) + 100) * 2 + 100 / 250'
+print(math_str + ' = ', end='')
+calculator = BracketsParser(Multiplication(Division(Substraction(Addition(ErrorHandler())))))
+result = calculator.parse(math_str)
+print(result)
